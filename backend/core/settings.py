eda231de976cb80873e926623714a4bac534a36e
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-19 21:40:20 +0600

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -86,7 +86,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.backends.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
