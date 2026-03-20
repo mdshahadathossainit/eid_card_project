6caf2ce9cb965ad6dc9477061f4f95cd931870ed
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-03-20 13:01:46 +0600

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.views.static import serve
+import os
+
+def home(request):
+    return HttpResponse("Eid Card Generator Backend is Running. Use /api/generate/ for POST requests.")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('generator.urls')),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
